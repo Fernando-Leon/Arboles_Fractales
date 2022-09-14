47dda03c5b9d0f7cc998d8d0b85dbf509f0c50f5
Fernando-Leon <leon.monroy.fernando@gmail.com>
2022-09-13 22:12:42 -0500

    Second commind

--- main.js
+++ main.js
@@ -1,14 +1,20 @@
+function setup() { frameRate(1);  createCanvas(600, 600); }
 
-function setup() { createCanvas(800, 900); }
+const canva = document.getElementById("defaultCanvas0");
+const containerCanva = document.getElementById("main-container");
+containerCanva.appendChild(canva);
 
 function draw() {
-    background("lightgray")
-    dibujarArbol(width / 2, height, 0, 100);
+    background("#000000")
+    console.log(height);
+    dibujarArbol(width / 2, height, 0, 70);
 }
+
 
 function dibujarArbol(x, y, angulo, radio) {
     if (radio < 1) return;
-    (radio < 5) ? stroke("green"): stroke("#320000");
+    (radio < 5) ? stroke("#e830ce")://Color hojas 
+    stroke("#FFFFFF"); //Color Tallo
 
     let x2 = x + radio * cos(angulo - PI / 2)
     let y2 = y + radio * sin(angulo - PI / 2)
@@ -20,3 +26,37 @@ function dibujarArbol(x, y, angulo, radio) {
     }
 
 }
+
+//Colors Dark  
+
+/*Color dark neon
+
+background: #000000
+colorPrimary: #FFFFFF
+
+colorsSeconday:
+
+violet1: #666f88
+green-blue: #2ef8a0
+yellow: #etc500
+pink: #f82d97
+pink2: #e830ce
+
+*/
+
+
+
+/*Color ligth
+
+background: #FFFFFF
+colorPrimary: #A18262
+
+colorsSeconday:
+
+green1: #2d572c
+green2: #00bb2
+azul1: #0000ff
+pink1: #ff0080
+pink2: #E63244
+violet: #830AC7
+*/
